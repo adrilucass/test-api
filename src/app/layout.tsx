5b68eeff4fbd25d6ec10bcfd1b92cbf19f372174
AdriLucas <adriel.agco@gmail.com>
2025-04-15 16:16:41 -0400

    feat: api-learn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black text-zinc-200 h-dvh">
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+'use client'
+
+import axios from "axios"
+import { Loader } from "lucide-react"
+import { useEffect, useState } from "react"
+
+interface dataProps {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string
+}
+
 export default function Home() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function getDataInAPi() {
+      try {
+        const get = await axios.get("https://jsonplaceholder.typicode.com/posts/1/comments")
+        setData(get.data)
+        setLoading(false)
+      } catch (err) {
+        console.error("nao deu bom")
+      }
+    }
+
+    getDataInAPi()
+  }, [])
+
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+    <div className="h-dvh flex justify-center items-center py-10">
+      <div className="bg-zinc-950 w-150 flex flex-col gap-2 justify-center items-center rounded-xl p-2">
+        {
+          loading ? <Loader className="animate-spin" /> : (
+            data.map((elements: dataProps) => (
+              <div key={elements.id} className="p-2 bg-zinc-900 rounded-md">
+                <h1 className="text-xl font-semibold">{elements.name}</h1>
+                <p className="text-zinc-400/70">{elements.email}</p>
+                <p className="text-zinc-400/70">{elements.body}</p>
+              </div>
+            ))
+          )
+        }
+      </div>
+    </div>
+  )
 }
